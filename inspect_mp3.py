7c59d2ecb729e6660864efe5f242baf91bd56edf
Robert-Yaman <robert.e.yaman@gmail.com>
2017-05-10 20:05:53 -0700

    MFCC creation

--- inspect_mp3.py
+++ inspect_mp3.py
@@ -4,33 +4,38 @@ import sys
 import matplotlib.pyplot as plt
 import os
 
-audio_path = os.getcwd() + '/'  + sys.argv[1]
+def mfccFromTimeSeries(timeSeries, sr):
+    return librosa.feature.mfcc(timeSeries, sr=sr)
 
-time_series, sampling_rate = librosa.load(audio_path)
+def mfccFromPath(audio_path):
+    times_series, sr = librosa.load(audio_path)
+    return mfccFromTimeSeries(times_series, sr)
 
-mfcc = librosa.feature.mfcc(time_series, sr=sampling_rate) # do I need to take log here?
+if __name__ == "__main__":
+    audio_path = os.getcwd() + '/'  + sys.argv[1]
 
-delta_mfcc  = librosa.feature.delta(mfcc)
-delta2_mfcc = librosa.feature.delta(mfcc, order=2)
+    mfcc = mfccFromPath(audio_path)
 
-plt.figure(figsize=(12, 6))
+    delta_mfcc  = librosa.feature.delta(mfcc)
+    delta2_mfcc = librosa.feature.delta(mfcc, order=2)
 
-plt.subplot(3,1,1)
-librosa.display.specshow(mfcc)
-plt.ylabel('MFCC')
-plt.colorbar()
+    plt.figure(figsize=(12, 6))
 
-plt.subplot(3,1,2)
-librosa.display.specshow(delta_mfcc)
-plt.ylabel('MFCC-$\Delta$')
-plt.colorbar()
+    plt.subplot(3,1,1)
+    librosa.display.specshow(mfcc)
+    plt.ylabel('MFCC')
+    plt.colorbar()
 
-plt.subplot(3,1,3)
-librosa.display.specshow(delta2_mfcc, sr=sampling_rate, x_axis='time')
-plt.ylabel('MFCC-$\Delta^2$')
-plt.colorbar()
+    plt.subplot(3,1,2)
+    librosa.display.specshow(delta_mfcc)
+    plt.ylabel('MFCC-$\Delta$')
+    plt.colorbar()
 
-plt.tight_layout()
+    plt.subplot(3,1,3)
+    librosa.display.specshow(delta2_mfcc, sr=sampling_rate, x_axis='time')
+    plt.ylabel('MFCC-$\Delta^2$')
+    plt.colorbar()
 
-plt.show()
+    plt.tight_layout()
 
+    plt.show()
--- midi_inspect.py
+++ midi_inspect.py
@@ -5,20 +5,20 @@ import copy
 
 # currently mutates
 def patternWithOneNoteTrackFromPattern(pattern):
-  # Takes a MIDI track with multiple note tracks (e.g. for a Bach fugure) and combines them. Returns a pattern with the unified track
+  # Takes a MIDI track with multiple note tracks (e.g. for a Bach fugue) and combines them. Returns a pattern with the unified track
 
   note_tracks = []
   for track in pattern:
     if type(track[0]) == midi.TextMetaEvent:
       note_tracks.append(track)
-  
+
   final_track = midi.Track()
   meta = midi.TextMetaEvent(tick=0, text='final', data=[49])
   final_track.append(meta)
 
   track_abs_times = {}
   for track in note_tracks:
-    track_abs_times[track[0].text] = 0 #confirm these are always unique 
+      track_abs_times[track[0].text] = 0 #TODO:confirm these are always unique 
   total_time = 0
   # get rid of the meta event tag in each tr ack
   # close - also end of track event
@@ -27,7 +27,7 @@ def patternWithOneNoteTrackFromPattern(pattern):
     best_score = float('inf') # we want low scores
     for itr_track in note_tracks:
       if len(itr_track) > 1:
-        if track_with_next_event == None or track_abs_times[itr_track[0].text] + itr_track[1].tick < best_score: 
+        if track_with_next_event == None or track_abs_times[itr_track[0].text] + itr_track[1].tick < best_score:
           track_with_next_event = itr_track
           best_score = track_abs_times[itr_track[0].text] + itr_track[1].tick
     # 0'th index is the meta event
@@ -35,7 +35,7 @@ def patternWithOneNoteTrackFromPattern(pattern):
 
     track_abs_times[track_with_next_event[0].text] += next_event.tick
     if type(next_event) == midi.NoteOnEvent or type(next_event) == midi.NoteOffEvent:
-      next_event.tick = track_abs_times[track_with_next_event[0].text] - total_time 
+      next_event.tick = track_abs_times[track_with_next_event[0].text] - total_time
       final_track.append(next_event)
     total_time = track_abs_times[track_with_next_event[0].text]
 
@@ -46,7 +46,10 @@ def patternWithOneNoteTrackFromPattern(pattern):
   pattern.append(final_track)
   return pattern
 
-# divide into measures	
+# Actually, we should just go straight into the representation we're using for our model here. Otherwise, the MIDI won't necessarily be valid (e.g. if there is a holdover note between measures).
+# Might be useful to be able to define ALL events into a single track for this.
+def DividePatternIntoMeasure(pattern):
+  # returns list of patterns 
 
 if __name__ == "__main__":
   midi_path = os.getcwd() + '/' + sys.argv[1]
