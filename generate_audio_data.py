570a189c4c952ca24fbf889b955d62a4077f2271
Robert Yaman <ryaman@ryaman.sbo.corp.google.com>
2017-05-30 20:48:13 -0700

    create mp3s

---
+++ generate_audio_data.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+TRAINING_DATA_PATH = './training_data/raw/'
+TRAINING_LABELS_PATH = './training_labels/raw/'
+
+def main():
+  for midi_name in os.listdir(TRAINING_DATA_PATH):
+    print 'Processing: ' + midi_name
+    base_name = os.path.splitext(midi_name)[0]
+    mp3_name = base_name + '.mp3'
+    print 'Creating: ' + mp3_name
+    subprocess.check_call("timidity '%s' -Ow -o - | lame - -b 64 '%s'" % (TRAINING_DATA_PATH + midi_name,
+                                                                          TRAINING_LABELS_PATH + mp3_name),
+                          shell=True)
+
+if __name__ == "__main__":
+  main()
