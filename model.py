19e1decc45f0dbdc6d2b1988f637c8899a2e4d34
Robert-Yaman <robert.e.yaman@gmail.com>
2017-06-04 23:17:49 -0700

    Training builds and runs, but immediately stops

--- model.py
+++ model.py
@@ -3,16 +3,8 @@ import numpy
 
 from tensorflow.python import debug as tf_debug
 
-DATA_DIR = 'small_dataset/'
-# TRAINING_STEPS * BATCH_SIZE has to be less than the amount of training data
-# we have.
-TRAINING_STEPS = 100
-BATCH_SIZE = 50
-
-
 # TODO: make number of MFCC units a parameter, since we will likely change
-
-def variable_summaries(var):
+def _variable_summaries(var):
     """Attach a lot of summaries to a Tensor (for TensorBoard
     visualization)."""
     with tf.name_scope('summaries'):
@@ -25,28 +17,18 @@ def variable_summaries(var):
         tf.summary.scalar('min',tf.reduce_min(var))
         tf.summary.histogram('histogram',var)
 
-def main(argv=None):
-    # Import data
-    training_labels = numpy.load(DATA_DIR + 'training_labels.npy')
-    # Data will have more data than labels, since data takes into account
-    # trailing silence in recordings. We trim this.
-    training_data = numpy.load(DATA_DIR +
-            'training_data.npy')[0:len(training_labels)]
-    validation_labels = numpy.load(DATA_DIR + 'validation_labels.npy')
-    validation_data = numpy.load(DATA_DIR +
-            'validation_data.npy')[0:len(validation_labels)]
-
+def get_transcription_model(examples):
     def weight_variables(shape):
         with tf.name_scope('weights'):
             initial = tf.truncated_normal(shape, stddev=.01)
             variables = tf.Variable(initial)
-            variable_summaries(variables)
+            _variable_summaries(variables)
             return variables
     def bias_variables(shape):
         with tf.name_scope('biases'):
             initial = tf.constant(0.1, shape=shape)
             variables = tf.Variable(initial)
-            variable_summaries(variables)
+            _variable_summaries(variables)
             return variables
     def conv_layer(inputs, filter_shape):
         with tf.name_scope('conv'):
@@ -57,11 +39,8 @@ def main(argv=None):
             return tf.nn.pool(inputs, window_shape=[2], strides=[2], pooling_type='MAX',
                     padding='SAME')
     with tf.name_scope('initialize'):
-        x_ = tf.placeholder(tf.float32, [None, 20])
         # Add a dimension to the input for multiple convolutional layers.
-        x = tf.expand_dims(x_, -1)
-        # Ouptuts - One hot encodings of currently sounding notes
-        y_ = tf.placeholder(tf.float32, [None, 88])
+        x = tf.cast(tf.expand_dims(examples, -1), tf.float32)
 
     with tf.name_scope('first_conv'):
         num_filters1 = 32
@@ -95,97 +74,10 @@ def main(argv=None):
             fc_layer_dropout = tf.nn.dropout(fc_layer, keep_prob)
 
     with tf.name_scope('readout'):
+        # TODO: Do I need to take sigmoid here since I'm not using softmax?
         weight_vars4 = weight_variables([num_nodes3, 88])
         bias_vars4 = bias_variables([88])
 
         readout = tf.matmul(fc_layer_dropout, weight_vars4) + bias_vars4
 
-    def interpretation(logit, cutoff):
-        # Returns a tensor that represents how we are interpreting the output
-        # of our model.
-        # |logit| is a [1, 88] tensor representing the likelihood that each
-        # note is present in a given sample.
-        # |cutoff| is the probability at which we will consider a note to be
-        # present.
-        with tf.name_scope('interpretation'):
-            return tf.cast(tf.greater(logit, cutoff), tf.float32)
-
-    with tf.Session() as sess:
-        if "debug" in argv:
-            sess = tf_debug.LocalCLIDebugWrapperSession(sess)
-            sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
-
-        # Don't use softmax since the outputs aren't mutually exclusive.
-        loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y_,
-                logits=readout))
-        with tf.name_scope('train'):
-            training_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-
-        # For now, analyze successfulness as number of predictions it gets
-        # exactly right. We can revisit this later.
-        with tf.name_scope('accuracy'):
-            with tf.name_scope('predictions'):
-                # For each note, did it get the right prediction?
-                correct_predictions7 = tf.cast(tf.equal(interpretation(readout,
-                    0.7), y_), tf.float32)
-                correct_predictions8 = tf.cast(tf.equal(interpretation(readout,
-                    0.8), y_), tf.float32)
-                correct_predictions9 = tf.cast(tf.equal(interpretation(readout,
-                    0.9), y_), tf.float32)
-
-                # Did it get the right prediction for every note? Look if the
-                # min value is 1.
-                correct_prediction7 = tf.equal(tf.argmin(correct_predictions7,
-                    1),1)
-                correct_prediction8 = tf.equal(tf.argmin(correct_predictions8,
-                    1),1)
-                correct_prediction9 = tf.equal(tf.argmin(correct_predictions9,
-                    1),1)
-
-            accuracy7 = tf.reduce_mean(tf.cast(correct_prediction7,
-                tf.float32))
-            accuracy8 = tf.reduce_mean(tf.cast(correct_prediction8,
-                tf.float32))
-            accuracy9 = tf.reduce_mean(tf.cast(correct_prediction9,
-                tf.float32))
-
-            tf.summary.scalar('accuracy7', accuracy7)
-            tf.summary.scalar('accuracy8', accuracy8)
-            tf.summary.scalar('accuracy9', accuracy9)
-
-        merged = tf.summary.merge_all()
-        train_writer = tf.summary.FileWriter('/tmp/tensorboard/train', sess.graph)
-        test_writer = tf.summary.FileWriter('/tmp/tensorboard/test')
-
-        sess.run(tf.global_variables_initializer())
-        for i in range(TRAINING_STEPS):
-            step_data = training_data[i * BATCH_SIZE:i * BATCH_SIZE + BATCH_SIZE]
-            step_labels = training_labels[i * BATCH_SIZE : i * BATCH_SIZE + BATCH_SIZE]
-
-            if i % 10 == 0:
-                #step_accuracy7 = accuracy7.eval(feed_dict={x_:step_data,
-                #    y_:step_labels, keep_prob:1})
-                #step_accuracy8 = accuracy8.eval(feed_dict={x_:step_data,
-                #    y_:step_labels, keep_prob:1})
-                #step_accuracy9 = accuracy9.eval(feed_dict={x_:step_data,
-                #    y_:step_labels, keep_prob:1})
-                #loss_val = loss.eval(feed_dict={x_:step_data,
-                #    y_:step_labels, keep_prob:1})
-                val_list = [merged, loss, accuracy7, accuracy8, accuracy9]
-                summary, loss_val, step_accuracy7, step_accuracy8, step_accuracy9  = sess.run(val_list, feed_dict={x_:step_data,
-                    y_:step_labels, keep_prob:1})
-
-                print('Step: %d:\n    Accuracy 0.7: %g\n    Accuracy 0.8: %g\n    Accuracy 0.9: %g\n    Loss: %g'%(i,
-                    step_accuracy7,step_accuracy8,step_accuracy9, loss_val))
-                test_writer.add_summary(summary, i)
-            else:
-                summary, _ = sess.run([merged, training_step], feed_dict={x_:step_data,
-                    y_:step_labels, keep_prob:0.5})
-                train_writer.add_summary(summary, i)
-            #training_step.run(feed_dict={x_:step_data, y_:step_labels,
-            #    keep_prob:0.5})
-
-    # TODO evaluate loss on validation data.
-
-if __name__ == '__main__':
-   tf.app.run()
+    return readout, keep_prob
---
+++ train.py
@@ -0,0 +1,128 @@
+import model
+import tensorflow as tf
+
+DATA_DIR = 'data/'
+TRAINING_DATA = DATA_DIR + 'training_data/training_data.csv'
+TRAINING_LABELS = DATA_DIR + 'training_labels/training_labels.csv'
+VALIDATION_DATA = DATA_DIR + 'validation_data/validation_data.csv'
+VALIDATION_LABELS = DATA_DIR + 'validation_labels/validation_labels.csv'
+
+BATCH_SIZE = 50
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+def _get_data(validation=False):
+    data_reader = tf.TextLineReader()
+    feature_file = tf.train.string_input_producer([VALIDATIOIN_DATA if validation else
+            TRAINING_DATA])
+    _, csv_row = data_reader.read(feature_file)
+    record_defaults = [[0]] * 20
+    features = tf.stack(list(tf.decode_csv(csv_row,
+        record_defaults=record_defaults)))
+
+    label_reader = tf.TextLineReader()
+    labels_file = tf.train.string_input_producer([VALIDATION_LABELS if validation else
+            TRAINING_LABELS])
+    _, csv_row = label_reader.read(labels_file)
+    record_defaults = [[0]] * 88
+    labels = tf.stack(list(tf.decode_csv(csv_row,
+        record_defaults=record_defaults)))
+
+    return features, labels
+
+def input_pipeline(validation=False):
+    example, label = _get_data(validation=validation)
+    min_after_dequeue = 10000
+    capacity = min_after_dequeue + 3 * BATCH_SIZE
+    example_batch, label_batch = tf.train.shuffle_batch(
+        [example, label], batch_size=BATCH_SIZE, capacity=capacity,
+        min_after_dequeue=min_after_dequeue)
+    label_batch = tf.cast(label_batch, tf.float32)
+    return example_batch, label_batch
+
+def main(argv=None):
+    with tf.Session() as sess:
+        train_writer = tf.summary.FileWriter('/tmp/tensorboard/train', sess.graph)
+        test_writer = tf.summary.FileWriter('/tmp/tensorboard/test')
+
+        if "debug" in argv:
+            sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+            sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
+        example_batch, label_batch = input_pipeline()
+        readout, keep_prob = model.get_transcription_model(example_batch)
+
+        # Don't use softmax since the outputs aren't mutually exclusive.
+        loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=label_batch,
+                logits=readout))
+        with tf.name_scope('train'):
+            training_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+
+        def interpretation(logit, cutoff):
+            # Returns a tensor that represents how we are interpreting the output
+            # of our model.
+            # |logit| is a [1, 88] tensor representing the likelihood that each
+            # note is present in a given sample.
+            # |cutoff| is the probability at which we will consider a note to be
+            # present.
+            with tf.name_scope('interpretation'):
+                return tf.cast(tf.greater(logit, cutoff), tf.float32)
+
+        # For now, analyze successfulness as number of predictions it gets
+        # exactly right. We can revisit this later.
+        with tf.name_scope('accuracy'):
+            with tf.name_scope('predictions'):
+                # For each note, did it get the right prediction?
+                correct_predictions7 = tf.cast(tf.equal(interpretation(readout,
+                    0.7), label_batch), tf.float32)
+                correct_predictions5 = tf.cast(tf.equal(interpretation(readout,
+                    0.5), label_batch), tf.float32)
+                correct_predictions9 = tf.cast(tf.equal(interpretation(readout,
+                    0.9), label_batch), tf.float32)
+
+                # Did it get the right prediction for every note? Look if the
+                # min value is 1.
+                correct_prediction7 = tf.equal(tf.argmin(correct_predictions7,
+                    1),1)
+                correct_prediction5 = tf.equal(tf.argmin(correct_predictions5,
+                    1),1)
+                correct_prediction9 = tf.equal(tf.argmin(correct_predictions9,
+                    1),1)
+
+            accuracy7 = tf.reduce_mean(tf.cast(correct_prediction7,
+                tf.float32))
+            accuracy5 = tf.reduce_mean(tf.cast(correct_prediction5,
+                tf.float32))
+            accuracy9 = tf.reduce_mean(tf.cast(correct_prediction9,
+                tf.float32))
+
+            tf.summary.scalar('accuracy7', accuracy7)
+            tf.summary.scalar('accuracy5', accuracy5)
+            tf.summary.scalar('accuracy9', accuracy9)
+
+        summary = tf.summary.merge_all()
+        sess.run(tf.global_variables_initializer())
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        try:
+            step = 0
+            while not coord.should_stop():
+                step += 1
+                if step % 1000:
+                    loss, summary = sess.run([loss, summary], feed_dict={keep_prob:1.0})
+                    print('Step: %d\n    Loss: %d' %(step, loss))
+                    train_writer.add_summary(summary, step)
+                else:
+                    _, summary = sess.run([training_step, summary],
+                            feed_dict={keep_prob:0.5})
+                    test_writer.add_summary(summary, step)
+        except tf.errors.OutOfRangeError:
+            print("DONE TRAINING")
+        finally:
+            coord.request_stop()
+
+if __name__ == '__main__':
+   tf.app.run()
