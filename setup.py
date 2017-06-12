f4eef7a7bf14c81636757350f69ecd57a7ea92f2
Robert Yaman <robert.e.yaman@gmail.com>
2017-06-12 11:19:16 -0700

    packaging for GCP

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='trainer',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='My trainer application package.'
+)
+
